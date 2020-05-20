6f3af5cf904a97665a8228e005b48c528d98d4c1
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2020-05-20 08:33:00 +0300

    added mobile menu

--- public/wp-content/themes/mytheme/spouse/header.php
+++ public/wp-content/themes/mytheme/spouse/header.php
@@ -18,10 +18,10 @@ wp_body_open();
 <header id="site-header" role="banner">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-2">
+            <div class="col-3">
             </div>
             <div class="col-5">
-                <div class="menu-container">
+                <div class="menu-container nav">
                   <?php wp_nav_menu( array( 'main_menu' => 'new-menu') ); ?>
                 </div>
             </div>
