737553c1b617b53bdfaa1d469fa49160a3d36fa9
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2020-05-27 09:49:10 +0300

    event list shortcode

--- public/wp-content/themes/mytheme/spouse/front-page.php
+++ public/wp-content/themes/mytheme/spouse/front-page.php
@@ -9,9 +9,6 @@ get_header();
               <div class="cta-background" style="background-image: url('<?php the_field('background'); ?>')">
                   <a href="<?php the_field('button_url'); ?>" class="<?php the_field('styles') ?>" ><?php echo the_field('button_text'); ?></a>
               </div>
-              <?php
-              #get_field('')
-              ?>
           </div>
           <div class="col-12 col-sm-12 col-lg-6 col-xl-5 main-content">
               <div class="main-content-container">
@@ -43,6 +40,7 @@ get_header();
           </div>
           <div class="col-12 col-sm-12 col-lg-6 col-xl-3 events-column">
               <h2>Upcoming events</h2>
+              <?php echo do_shortcode('[spouse-events]'); ?>
           </div>
       </div>
   </div>
--- public/wp-content/themes/mytheme/spouse/functions.php
+++ public/wp-content/themes/mytheme/spouse/functions.php
@@ -38,3 +38,67 @@ function spouse_add_shortcodes() {
 function spouse_login_form_shortcode() {
   return wp_login_form( array( 'echo' => false ) );
 }
+
+function spouse_get_events(){
+  $args = [
+    'post_type' => 'eventbrite_events',
+  ];
+  #get_posts($args);
+
+  $posts = wp_get_recent_posts($args, OBJECT);
+
+  foreach($posts as $post){
+
+    $color = '#fff';
+    $startDate = null;
+
+    $terms = get_the_terms($post->ID, 'eventbrite_category');
+
+    if(is_wp_error($terms) && $terms !== FALSE){
+      return $terms->get_error_message();
+    }
+
+    $icon = get_field('icon', $post->ID);
+
+    if($term = reset($terms)) {
+      $color = acf_get_field('event_color', $term);
+      $category = $term->name;
+    }
+
+    if($dateData = get_post_meta($post->ID, 'event_start_date')) {
+
+      $date = new \DateTime();
+      $date->setTimestamp(strtotime($dateData[0]));
+      $startDate = $date->format('d/m');
+
+      $hour = get_post_meta($post->ID, 'event_start_hour')[0];
+      $minute = get_post_meta($post->ID, 'event_start_minute')[0];
+      $meridian = get_post_meta($post->ID, 'event_start_meridian')[0];
+      $startTime = "$hour:$minute $meridian";
+    }
+
+    ?>
+    <div class="event">
+    <?php if(is_user_logged_in()): ?>
+      <a href="<?php echo get_permalink($post) ?>">
+    <?php endif; ?>
+          <div class="event-color" <?php if(isset($color) && $color): ?>style="background-color:<?php echo $color['value']; ?>" <?php endif; ?>></div>
+          <div class="event-content">
+              <div class="event-start"><?php echo $startDate; ?></div>
+              <div class="text-content">
+                <span class=""><?php echo $category ?></span>
+                <p><?php echo $startTime; ?> @ <?php echo get_post_meta($post->ID, 'venue_name')[0]; ?></p>
+                <p><?php echo get_post_meta($post->ID, 'venue_address')[0]; ?> <?php echo get_post_meta($post->ID, 'venue_city')[0]; ?></p>
+              </div>
+          </div>
+          <div class="event-icon"><img src="<?php echo $icon ?>"></div>
+    <?php if(is_user_logged_in()): ?>
+      </a>
+    <?php endif; ?>
+    </div>
+    <?php
+
+  }
+}
+
+add_shortcode('spouse-events', 'spouse_get_events');
